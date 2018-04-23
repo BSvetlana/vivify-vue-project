f402d5c9c9b86d1e41cfaec2dd55f8e61fe42266
BSvetlana <svetlanabisacki@gmail.com>
2018-04-23 21:00:03 +0200

    "contacts table"

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContanctsList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContanctsList from './components/ContanctsList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      ContanctsList
   }
 }
 </script>
---
+++ src/components/ContanctsList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+       <table>
+           <thead>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+           </thead>
+           <tbody  v-for="(contact,key) in contacts" :key="key" >
+           <tr>
+               <td>
+                   {{contact.firstName}}
+               </td>
+               <td>
+                   {{contact.lastName}}
+               </td>
+               <td>
+                   {{contact.email}}
+               </td>
+           </tr>
+           </tbody>
+
+       </table>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "ContanctsList",
+        data(){
+
+            return {
+                contacts: [
+                    {firstName: 'John', lastName: 'Jonson', email: 'john@jonhson.com'},
+                    {firstName: 'Don', lastName: 'Donson', email: 'don@donson.com'},
+                    {firstName: 'Stiv', lastName: 'Stivenson', email: 'stiv@stivenson.com'}
+                ]
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
