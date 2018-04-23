1690516621035ef09a1d572097ed900502c8256b
BSvetlana <svetlanabisacki@gmail.com>
2018-04-23 21:39:42 +0200

    "form add"

--- src/components/ContanctsList.vue
+++ src/components/ContanctsList.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
+        <form @submit.prevent ="addContact">
+
+            <label>First Name</label>
+            <input type="text" placeholder="First Name" v-model="newContact.firstName"/>
+
+            <label>Last Name</label>
+            <input type="text" placeholder="Last Name" v-model="newContact.lastName"/>
+
+            <label>Email</label>
+            <input type="email" placeholder="Your Email" v-model="newContact.email"/>
+
+            <button type="submit">Add Contact</button>
+        </form>
        <table>
            <thead>
                 <th>First Name</th>
@@ -7,7 +20,7 @@
                 <th>Email</th>
            </thead>
            <tbody>
-           <tr v-for="(contact,key) in contacts" :key="key" v-if="contact.firstName === 'John'">
+           <tr v-for="(contact,key) in contacts" :key="key">
                <td>
                    {{contact.firstName}}
                </td>
@@ -35,8 +48,19 @@
                     {firstName: 'John', lastName: 'Jonson', email: 'john@jonhson.com'},
                     {firstName: 'Don', lastName: 'Donson', email: 'don@donson.com'},
                     {firstName: 'Stiv', lastName: 'Stivenson', email: 'stiv@stivenson.com'}
-                ]
+                ],
 
+                newContact: {
+                    firstName: '',
+                    lastName: '',
+                    email: ''
+                }
+            }
+        },
+        methods: {
+            addContact(){
+                this.contacts.push(this.newContact);
+                this.newContact = {}
             }
         }
     }
